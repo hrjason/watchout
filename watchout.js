6359bfc9bbf01fed7f89bd536d75896eddfead5c
jasonjchang <hrjasonchang@gmail.com>
2015-02-14 14:42:32 -0800

    COMPLETED MUTHAFAWKA

--- watchout.js
+++ watchout.js
@@ -4,7 +4,7 @@ var gameObject = {
   height  : 450 ,
   width   : 700,
   nEnemies: 30,
-  padding : 5,
+  padding : 0,
   deathTimer: 0
 };
 
@@ -26,6 +26,7 @@ var axesReverse = {
 
 
 var gameBoard = d3.select('.container').append('svg')
+                  .attr('class', 'gameboard')
                   .attr('width', gameObject.width + "px")
                   .attr('height', gameObject.height + "px")
                   .style('border', '1px solid black')
@@ -59,13 +60,13 @@ gameBoard.selectAll('.enemy').data(createEnemies(gameObject.nEnemies))
                         })
                         .attr('r', 10)
                         .transition().duration(1000)
-                        .style('opacity', .6)
+                        .style('opacity', 1)
 
 var player = {
   fill: 'red',
-  x: 50,
-  y: 50,
-  r: 10
+  x: 0,
+  y: 0,
+  r: 20
 };
 
 var dragstarted = function(d) {
@@ -73,9 +74,17 @@ var dragstarted = function(d) {
   d3.select(this).classed('dragging', true);
 };
 
-var dragged = function(d) {
-  d3.select(this).attr('cx', d.x = Math.max(axes.x(gameObject.padding), Math.min(gameObject.width-axes.x(gameObject.padding), d3.event.x)))
-                .attr('cy', d.y = Math.max(axes.y(gameObject.padding), Math.min(gameObject.height-axes.y(gameObject.padding), d3.event.y)));
+var dragged = function(d, i) {
+  d.x += d3.event.dx;
+  d.y += d3.event.dy;
+  player.x = d.x;
+  player.y = d.y;
+  d3.select(this)
+    .attr('transform', function(d, i){
+      return 'translate(' + [ Math.max(0, Math.min(671, d.x)), Math.max(0, Math.min(421, d.y)) ] +')'
+    });
+  // d3.select(this).attr('cx', d.x = Math.max(axes.x(gameObject.padding), Math.min(gameObject.width-axes.x(gameObject.padding), d3.event.x)))
+  //               .attr('cy', d.y = Math.max(axes.y(gameObject.padding), Math.min(gameObject.height-axes.y(gameObject.padding), d3.event.y)));
 };
 
 var dragended = function(d) {
@@ -90,25 +99,32 @@ var drag = d3.behavior.drag()
 
 
 gameBoard.selectAll('.pacman').data([player])
-  .enter().append('circle')
-    .attr('class', 'pacman')
-    .attr('cx', function(d) { return axes.x(d.x) + 'px'; })
-    .attr('cy', function(d) { return axes.y(d.y) + 'px'; })
-    .attr('r', function(d) { return d.r; })
-    .attr('fill', '#faf703')
-    .call(drag);
+  .enter()
+      .append('path')
+      .attr('class', 'pacman')
+      .attr('d', "M"+(53.5441/2)+","+(41.2339/2)+"A"+(28.0868/2)+","+(28.0868/2)+" 0 1,1 "+(53.6186/2)+","+(16.1733/2)+"L"+(28.4493/2)+","+(28.629/2)+"Z")
+      //.attr('d', "M134.042,128.971l115.096-17.214C241.636,51.55,190.281,4.958,128.042,4.958C60.641,4.958,6,59.599,6,127
+      //s54.641,122.042,122.042,122.042c60.942,0,111.447-44.671,120.569-103.054L134.042,128.971z")
+      //.attr('cx', function(d) { return axes.x(d.x) + 'px'; })
+      //.attr('cy', function(d) { return axes.y(d.y) + 'px'; })
+      //.attr('r', function(d) { return d.r; })
+      .attr('fill', '#ffcc00')
+      .style('transform', function(d) {
+        return 'translate(' + 50 + ', ' + 50 + ')';
+      })
+      .call(drag);
 
 var collisionCheck = function() {
-  var pacmanX = parseFloat(gameBoard.select('.pacman').attr('cx'));
-  var pacmanY = parseFloat(gameBoard.select('.pacman').attr('cy'));
+  // var pacmanX = parseFloat(gameBoard.select('.pacman').attr('cx'));
+  // var pacmanY = parseFloat(gameBoard.select('.pacman').attr('cy'));
   var enemies = gameBoard.selectAll('.enemy');
   var collides = false;
   enemies.each(function(enemy) {
     var enemy = d3.select(this);
     var distance = Math.sqrt(
-                          Math.pow((parseFloat(enemy.attr('cy')) - pacmanY), 2)
+                          Math.pow((parseFloat(enemy.attr('cy')) - player.y), 2)
                           +
-                          Math.pow((parseFloat(enemy.attr('cx')) - pacmanX), 2)
+                          Math.pow((parseFloat(enemy.attr('cx')) - player.x), 2)
                           );
     if(distance < (player.r * 2)){
       collides = true;
